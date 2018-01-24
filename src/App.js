ab3e424724da9618afd5f3f8e35e0e431ff860d2
ZhouBin <jobn_joy@163.com>
2018-01-24 15:44:25 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import fetch from 'node-fetch'
 import cc from 'cryptocompare'
 import { observable, useStrict, action } from 'mobx';
 import { observer } from 'mobx-react';
+import ContentLoader from 'react-content-loader'
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend} from 'recharts';
 
 import newState from './store'
 
 import logo from './logo.svg';
+import { link } from 'fs';
 import './App.css';
 
 @observer
@@ -15,25 +18,75 @@ class App extends Component {
 
   componentWillMount() {
     newState.fetchApi()
-    // cc.priceMulti(['BTC', 'ETH', 'XRP', 'LTC', 'DASH', 'ZEC'], ['USD', 'EUR'])
-    // .then(prices => {
-    //   debugger
-    // })
+  }
+  showSection(key) {
+    // newState.histoDay(key)
+    newState.showHideLine(() => {
+      if (newState.showLine) {
+        newState.histoDay(key)
+      }
+    })
+  }
+  renderCoinLists() {
+    const prices = newState.obj
+    const liArr = []
+    for (const key in prices) {
+      if (prices.hasOwnProperty(key)) {
+        const element = prices[key]
+        liArr.push(<li key={ key } onClick={ () => {this.showSection(key)}}>
+          <span>{ key }</span>
+          <span className="usdprice">{ element.USD }$</span>
+        </li>)
+      }
+    }
+    return (
+      <div>
+        <ul className="priceul">
+          { liArr }
+        </ul>
+      </div>
+    )
+  }
+  renderCharts() {
+    if (!newState.showLine) return
+
+    const data = newState.hisArr
+    return (
+      <LineChart width={730} height={250} data={data}
+          margin={{ top: 5, right: 10, left: 20, bottom: 5 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="time" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="high" stroke="#8884d8" />
+          <Line type="monotone" dataKey="low" stroke="#82ca9d" />
+        </LineChart>
+    )
   }
   render() {
-    debugger
-    console.log(newState.obj)
+    const MyLoader = () => (
+      <ContentLoader>
+        {/* Pure SVG */}
+        <rect x="0" y="0" rx="4" ry="4" width="100%" height="30" />
+        <rect x="0" y="35" rx="4" ry="4" width="100%" height="30" />
+        <rect x="0" y="70" rx="3" ry="3" width="100%" height="30" />
+        <rect x="0" y="105" rx="3" ry="3" width="100%" height="30" />
+        <rect x="0" y="140" rx="3" ry="3" width="100%" height="30" />
+        <rect x="0" y="175" rx="3" ry="3" width="100%" height="30" />
+      </ContentLoader>
+    )
+    if (newState.priceFlag) {
+      return (
+        <div className="App">
+          { this.renderCoinLists() }
+          { this.renderCharts() }
+        </div>
+      );
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <MyLoader />
+    )
   }
 }
 
--- src/store.js
+++ src/store.js
@@ -5,25 +5,30 @@ import cc from 'cryptocompare'
 useStrict(true);
 class Store {
   @observable obj = {};
-  @observable number = 0;
-  @action add = () => {
-    this.number++;
+  @observable hisArr = [];
+  @observable priceFlag = false;
+  @observable showLine = false;
+  @observable canshowLine = false;
+
+  @action showHideLine = (cb) => {
+    this.showLine = !this.showLine
+    cb()
   }
   @action fetchApi = async () => {
-    const prices = await cc.priceMulti(['BTC', 'ETH', 'XRP', 'LTC', 'DASH', 'ZEC'], ['USD', 'EUR'])
+    const prices = await cc.priceMulti(['BTC', 'ETH', 'XRP', 'LTC', 'DASH', 'ZEC'], ['USD'])
     runInAction( () => {
+      this.priceFlag = true
       this.obj = prices
     })
-    // cc.priceMulti(['BTC', 'ETH', 'XRP', 'LTC', 'DASH', 'ZEC'], ['USD', 'EUR'])
-    // .then(prices => {
-    //   debugger
-    // })
-    // const response = await fetch('http://47.93.83.7:8000/index2/?format=json&platform=mobile')
-    // const json = await response.json()
-    // runInAction( () => {
-    //   this.obj = json.genres
-    // })
-  } 
+  }
+  @action histoDay = async (key) => {
+    this.canshowLine = false
+    const arr = await cc.histoDay(key, 'USD')
+    runInAction( () => {
+      this.canshowLine = true
+      this.hisArr = arr
+    })
+  }
 }
 
 const newStore = new Store();
